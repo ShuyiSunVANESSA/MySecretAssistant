facce3ee74381a608e75e4bacc35c1fdb5cce7f7
AgMac <angelia.gyq@gmail.com>
2020-06-02 23:01:29 -0400

    set index page to display readme

--- period_registry/__init__.py
+++ period_registry/__init__.py
@@ -1,5 +1,6 @@
 # Import the framework
 from flask import Flask
+import markdown
 import os
 
 # Create and instance of Flask
@@ -7,4 +8,15 @@ app = Flask (__name__)
 
 @app.route("/")
 def index():
-    return "aloha!"
+    """Present some documentation"""
+
+    # Open the README file
+    readme_file = os.path.dirname(app.root_path) + '/README.md'
+    print("HIHIHIHIHI", readme_file)
+    with open(readme_file, 'r') as markdown_file:
+
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
